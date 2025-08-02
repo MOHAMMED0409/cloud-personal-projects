2dc9d1557097b2a70e843a1a971cc6e61f0d9ffc
MOHAMMED0409 <mohammedkhashif02@gmail.com>
2025-08-02 16:51:59 +0530

    breaked terraform scripts

--- project-1/2048-ci-cd-aws/terraform/outputs.tf
+++ project-1/2048-ci-cd-aws/terraform/outputs.tf
@@ -1,11 +1,7 @@
-output "ecr_url" {
-  value = aws_ecr_repository.repo.repository_url
+output "ecr_repo_url" {
+  value = aws_ecr_repository.game_repo.repository_url
 }
 
 output "ecs_service_name" {
-  value = aws_ecs_service.fargate_service.name
+  value = aws_ecs_service.app.name
 }
-
-# output "codecommit_repo_url" {
-#   value = aws_codecommit_repository.repo.clone_url_http
-# }
--- project-1/2048-ci-cd-aws/terraform/variables.tf
+++ project-1/2048-ci-cd-aws/terraform/variables.tf
@@ -1,5 +1,30 @@
-variable "region"         { default = "us-east-1" }
-variable "project_name"   { default = "2048-game" }
-variable "ecr_repo_name"  { default = "2048-ecr-repo" }
-variable "ecs_cluster"    { default = "2048-cluster" }
-variable "container_port" { default = 80 }
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "github_owner" {
+  default = "MOHAMMED0409"
+}
+
+variable "github_repo" {
+  default = "cloud-personal-projects"
+}
+
+variable "github_branch" {
+  default = "main"
+}
+
+variable "github_token" {
+  type        = string
+  description = "GitHub OAuth token for CodePipeline"
+  sensitive   = true
+}
+
+variable "codestar_connection_arn" {
+  description = "ARN of the GitHub CodeStar connection"
+  type        = string
+}
+variable "project_name" {
+  description = "The name of the project to use for resource naming"
+  type        = string
+}
